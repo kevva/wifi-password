5ca4b7b2a84c4d5cc0510a6dd58650c9d555a1b3
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-16 23:17:33 +0100

    Add support for specifying a specific SSID
    Fixes #1.

--- index.js
+++ index.js
@@ -3,12 +3,46 @@
 var exec = require('child_process').exec;
 var wifiname = require('wifi-name');
 
-module.exports = function (cb) {
+function getPassword(name, cb) {
 	var cmd;
 	var ret;
 
+	if (process.platform === 'darwin') {
+		cmd = 'security find-generic-password -ga "' + name + '"';
+	} else if (process.platform === 'linux') {
+		cmd = 'sudo cat /etc/NetworkManager/system-connections/"' + name + '"';
+	}
+
+	exec(cmd, function (err, stdout) {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		if (stdout && process.platform === 'darwin') {
+			ret = /^\s*password: (.+)\s*$/gm.exec(stdout);
+			ret = ret && ret.length ? ret[1] : null;
+		}
+
+		if (stdout && process.platform === 'linux') {
+			ret = /^\s*psk=(.+)\s*$/gm.exec(stdout);
+			ret = ret && ret.length ? ret[1] : null;
+		}
+
+		cb(null, ret);
+	});
+}
+
+module.exports = function (ssid, cb) {
 	if (process.platform !== 'darwin' && process.platform !== 'linux') {
 		throw new Error('Only OS X and Linux systems are supported');
+	}
+
+	if (typeof ssid !== 'function') {
+		getPassword(ssid, cb);
+		return;
+	} else {
+		cb = ssid;
 	}
 
 	wifiname(function (err, name) {
@@ -17,29 +51,6 @@ module.exports = function (cb) {
 			return;
 		}
 
-		if (process.platform === 'darwin') {
-			cmd = 'security find-generic-password -ga "' + name + '"';
-		} else if (process.platform === 'linux') {
-			cmd = 'sudo cat /etc/NetworkManager/system-connections/"' + name + '"';
-		}
-
-		exec(cmd, function (err, stdout) {
-			if (err) {
-				cb(err);
-				return;
-			}
-
-			if (stdout && process.platform === 'darwin') {
-				ret = /^\s*password: (.+)\s*$/gm.exec(stdout);
-				ret = ret && ret.length ? ret[1] : null;
-			}
-
-			if (stdout && process.platform === 'linux') {
-				ret = /^\s*psk=(.+)\s*$/gm.exec(stdout);
-				ret = ret && ret.length ? ret[1] : null;
-			}
-
-			cb(null, ret);
-		});
+		getPassword(name, cb);
 	});
 };
