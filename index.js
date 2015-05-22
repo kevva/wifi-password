1faac1b7b8512efd52ffe0138920a3838674dc9a
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-22 19:06:27 +0200

    Typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.platform === 'darwin') {
-	module.exports = require('osx-wifi-passwrd');
+	module.exports = require('osx-wifi-password');
 } else if (process.platform === 'win32') {
 	module.exports = require('win-wifi-password');
 } else {
